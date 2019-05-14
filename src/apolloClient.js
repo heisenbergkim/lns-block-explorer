2dc7683adc9a426eb969d93f88d532c2ad4156a1
EricKim <erickim000@gmail.com>
2019-05-14 12:39:59 +0900

    change url in apollocient.js

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -6,7 +6,7 @@ import ApolloClient from "apollo-boost";
 // };
 
 const client = new ApolloClient({
-  uri: "http://192.168.153.129:4000/graphql"
+  uri: "http://119.193.191.176:4000/graphql"
   // uri: "http://localhost:4000"
 });
 
